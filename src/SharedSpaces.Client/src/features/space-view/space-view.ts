3bfbf790d563898c30a6fea940290079eba21c18
Marek Fišera <mara@neptuo.com>
2026-03-21 16:01:34 +0100

    Fix PRE formatted element leading spaces

--- src/SharedSpaces.Client/src/features/space-view/space-view.ts
+++ src/SharedSpaces.Client/src/features/space-view/space-view.ts
@@ -1180,9 +1180,7 @@ export class SpaceView extends BaseElement {
               <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>
             </button>
           </div>
-          <p class="whitespace-pre-wrap break-words text-start text-sm text-slate-200">
-            ${this.modalItem.content}
-          </p>
+          <p class="whitespace-pre-wrap break-words text-start text-sm text-slate-200">${this.modalItem.content}</p>
         </div>
       </div>
     `;
